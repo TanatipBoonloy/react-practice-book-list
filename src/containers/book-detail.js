b122086fd040d84c560e30a64e56065c3f6f631c
Tanatip Boonloy <Tanatip Boonloy>
2017-02-15 19:46:35 +0700

    complete booklist with action-reducer tutorial

---
+++ src/containers/book-detail.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+    render() {
+        const {book} = this.props;
+        return (
+            <div>
+              { book ?
+                <div>
+                  <h3>Details for:</h3>
+                  <div>
+                    Title : 
+                    { book.title }
+                  </div>
+                  <div>
+                    Pages : 
+                    { book.pages } 
+                  </div>
+                </div>
+                :
+                <div>
+                  Select a book to get started.
+                </div> }
+            </div>
+            );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        book: state.activeBook
+    }
+}
+
+export default connect(mapStateToProps)(BookDetail);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,20 @@
+export default function() {
+    return [
+        {
+            title: 'Javascript: The Good Parts',
+            pages: 101
+        },
+        {
+            title: 'Harry Potter',
+            pages: 100
+        },
+        {
+            title: 'The Dark Tower',
+            pages: 120
+        },
+        {
+            title: 'Eloquent Ruby',
+            pages: 10
+        },
+    ]
+}
